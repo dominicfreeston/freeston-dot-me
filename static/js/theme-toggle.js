7eff6c46abd91775a7be3fc376efcfbe1e19c60b
Dominic <dominic.freeston@gmail.com>
2022-05-16 14:34:42 +0100

    Store theme to local-storage

--- static/js/theme-toggle.js
+++ static/js/theme-toggle.js
@@ -1,14 +1,26 @@
 const btn = document.querySelector("#theme-toggle");
 const root = document.documentElement;
 
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+    root.classList.add(currentTheme);
+}
+
 btn.addEventListener("click", function() {
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
     
     if (prefersDarkScheme.matches) {
 	root.classList.toggle("light-theme");
 	root.classList.remove("dark-theme");
-  } else {
-      root.classList.toggle("dark-theme");
-      root.classList.remove("light-theme");
-  }
+    } else {
+	root.classList.toggle("dark-theme");
+	root.classList.remove("light-theme");
+    }
+
+    const newTheme = root.classList[0];
+    if (newTheme) {
+	localStorage.setItem("theme", newTheme);
+    } else {
+	localStorage.removeItem("theme");
+    }
 });
